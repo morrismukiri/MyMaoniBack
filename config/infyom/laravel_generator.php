47b0d2f678476b07aae60a53d6b71140db4e7ff7
Morris Mukiri <morrismukiri@gmail.com>
2017-03-21 23:49:27 +0300

    swagger docs and datatables

--- config/infyom/laravel_generator.php
+++ config/infyom/laravel_generator.php
@@ -140,11 +140,11 @@ return [
 
     'add_on' => [
 
-        'swagger'       => false,
+        'swagger'       => true,
 
         'tests'         => true,
 
-        'datatables'    => false,
+        'datatables'    => true,
 
         'menu'          => [
 
