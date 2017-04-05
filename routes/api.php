abeef1ea2d93d8c2ded577122d38d04eec3f5a2b
Morris Mukiri <morrismukiri@gmail.com>
2017-04-06 00:02:57 +0300

    Sample get user details from JWT header

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::get('/user', function (Request $request) {
 Route::post('/authenticate', 'AuthenticateController@authenticate');
 Route::resource('polls', 'PollAPIController');
 
-
+Route::get('/user/details','AuthenticateController@getAuthenticatedUser');
 
 
 
