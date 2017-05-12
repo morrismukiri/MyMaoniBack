e563f95f5535673d8e94a8a82d852f89c711239e
Morris Mukiri <morrismukiri@gmail.com>
2017-05-12 03:02:47 +0300

    show survey polls on view

--- resources/views/surveys/show_fields.blade.php
+++ resources/views/surveys/show_fields.blade.php
@@ -46,4 +46,40 @@
     {!! Form::label('updated_at', 'Updated At:') !!}
     <p>{!! $survey->updated_at !!}</p>
 </div>
+<h2>Polls</h2>
+<div class="list-group">
+    @foreach($survey->polls as $poll)
+
+        <div class="list-group-item">
+            <h3 class="list-group-item-heading">
+                {{$poll->title}}
+                {!! Form::open(['route' => ['polls.destroy', $poll->id], 'method' => 'delete','class'=>'pull-right']) !!}
+
+
+                <div class='btn-group'>
+                    <a href="{!! route('polls.show', [$poll->id]) !!}" class='btn btn-default '
+                       title="View"><i class="glyphicon glyphicon-tasks"></i></a>
+                    <a href="{!! route('polls.show', [$poll->id]) !!}" class='btn btn-default '
+                       title="View"><i class="glyphicon glyphicon-eye-open"></i></a>
+                    <a href="{!! route('polls.edit', [$poll->id]) !!}" class='btn btn-default '
+                       title="Edit"><i class="glyphicon glyphicon-edit"></i></a>
+                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger ', 'title'=>'Delete', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                </div>
+                {!! Form::close() !!}
+            </h3>
+
+
+            <div class="list-group-item-text">
+                <ul class=" list-group">
+                    @foreach($poll->answers as $answer)
+                        <li class="list-group-item ">{{ $answer->text }}</li>
+                    @endforeach
+                </ul>
+            </div>
+
+        </div>
+    @endforeach
+
+
+</div>
 
