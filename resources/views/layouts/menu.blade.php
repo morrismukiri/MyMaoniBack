5fb3a9d32155eea0a08156fd4a50b14cfcbf5f50
Morris Mukiri <morrismukiri@gmail.com>
2017-08-26 02:00:45 +0300

    Remove comment duplicate in menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -32,7 +32,3 @@
     <a href="{!! route('comments.index') !!}"><i class="fa fa-edit"></i><span>Comments</span></a>
 </li>
 
-<li class="{{ Request::is('comments*') ? 'active' : '' }}">
-    <a href="{!! route('comments.index') !!}"><i class="fa fa-edit"></i><span>Comments</span></a>
-</li>
-
