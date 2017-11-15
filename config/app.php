bf977c632bc563b4a5054aa5deb63ae652ca0e2f
Morris Mukiri <morrismukiri@gmail.com>
2017-11-16 02:06:45 +0300

    Swagger

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         \InfyOm\Generator\InfyOmGeneratorServiceProvider::class,
         InfyOm\AdminLTETemplates\AdminLTETemplatesServiceProvider::class,
+        Jlapp\Swaggervel\SwaggervelServiceProvider::class,
         /*
          * Application Service Providers...
          */
