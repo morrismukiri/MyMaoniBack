42b994328d2a6a177058959dccefd504f36908ed
Morris Mukiri <morrismukiri@gmail.com>
2017-11-20 23:58:44 +0300

    Format

--- routes/web.php
+++ routes/web.php
@@ -13,45 +13,44 @@
 
 Route::get('/', function () {
     return redirect('home');
-});
+}); 
 
 
-Auth::routes();
+Auth::routes(); 
 
-Route::get('/home', 'HomeController@index');
+Route::get('/home', 'HomeController@index'); 
 
-Route::get('/ps', 'HomeController@ps');
+Route::get('/ps', 'HomeController@ps'); 
 
-Route::resource('users', 'UserController');
+Route::resource('users', 'UserController'); 
 
-Route::resource('polls', 'PollController');
-Route::get('polls/create/{survey?}', 'PollController@create');
-
-
+Route::resource('polls', 'PollController'); 
+Route::get('polls/create/{survey?}', 'PollController@create'); 
 
 
 
-Route::resource('opinions', 'OpinionController');
 
-Route::resource('categories', 'CategoryController');
 
-Route::resource('answers', 'AnswersController');
-Route::get('answers/create/{pollId?}', 'AnswersController@create');
+Route::resource('opinions', 'OpinionController'); 
 
-Route::resource('surveys', 'SurveyController');
+Route::resource('categories', 'CategoryController'); 
+
+Route::resource('answers', 'AnswersController'); 
+Route::get('answers/create/{pollId?}', 'AnswersController@create'); 
+
+Route::resource('surveys', 'SurveyController'); 
 
 //Survey routes
 
-Route::get('/survey/{survey}/polls','SurveyController@SurveyPolls');
-Route::get('/poll/{poll}/answers','PollController@PollAnswers');
-Route::get('/survey/{survey}/results','SurveyController@SurveyResults');
+Route::get('/survey/{survey}/polls', 'SurveyController@SurveyPolls'); 
+Route::get('/poll/{poll}/answers', 'PollController@PollAnswers'); 
+Route::get('/survey/{survey}/results', 'SurveyController@SurveyResults'); 
 
 
-Route::resource('comments', 'CommentsController');
+Route::resource('comments', 'CommentsController'); 
 
-Route::resource('comments', 'CommentsController');
+Route::resource('comments', 'CommentsController'); 
 
-Route::get('/passport',function ()
-{
-    return view("passport_components");
-})->name('passport');
+Route::get('/passport', function () {
+    return view("passport_components"); 
+})->name('passport'); 
